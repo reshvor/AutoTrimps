aa662c49f2b393af6f75877618c1d6e895556164
reshvor <reshvor@gmail.com>
2023-07-30 13:18:50 -0500

    Update mods.js

--- mods.js
+++ mods.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Zek';
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://github.com/reshvor/AutoTrimps/blob/gh-pages/AutoTrimps2.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
 
